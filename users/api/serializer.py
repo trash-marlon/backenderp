3f5606fcc625253b22d64564465e83e4ad5be56c
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-01 23:27:38 +0100

    sc

--- users/api/serializer.py
+++ users/api/serializer.py
@@ -27,3 +27,16 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- users/api/views.py
+++ users/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from users.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from users.api.serializer import UserRegisterSerializer, UserSerializer
+from users.api.serializer import UserRegisterSerializer, UserSerializer, UserUpdateSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(APIView):
@@ -21,3 +21,10 @@ class UserView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        serializer = UserUpdateSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
