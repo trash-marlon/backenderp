8490b0f2e2c8faabbc62c4d72d1fe75d7d39a56b
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-04 21:34:10 +0100

    sc

--- users/models.py
+++ users/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+SALE_PERMISSION = (
+        ('own', 'own'),
+        ('all', 'all'),
+        ('admin', 'admin'),
+    )
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
+    sale_permission = models.CharField(
+        choices=SALE_PERMISSION, max_length=100, default='admin')
+    
 
     # If you whant to create a supersuper comment this line.
     USERNAME_FIELD = 'email'
