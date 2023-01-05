bafb205d7bdcb1c64e3fac7e36e00f805d3d0028
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-05 18:49:10 +0100

    sc

--- conf/api/views/configuration.py
+++ conf/api/views/configuration.py
@@ -1,9 +1,38 @@
 from rest_framework.viewsets import ModelViewSet
-from conf.models import Configuration
 from conf.api.serializers import ConfigurationSerializer
 from rest_framework.permissions import IsAuthenticated
+from conf.models import Configuration
+# Load Models
+from con.models.contact import Country
 
 class ConfigurationViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = ConfigurationSerializer
     queryset = Configuration.objects.all()
+
+    def LoadData():
+        try:
+            conf = Configuration.objects.get(pk=1)
+        except Configuration.DoesNotExist:
+            print("load data from file csv")
+            conf = Configuration()
+            conf.id = 1
+            conf.name = "Configuration"
+            conf.uc_id = 1
+
+            # Load all data from file csv
+            # 1. Load country from data/country.csv
+            csv =  open('con/data/country.csv', 'r')
+            
+            for line in csv:
+                country = Country()
+                country.name =  line.split(',')[0]
+                country.code =  line.split(',')[1]
+                country.uc_id = 1
+                country.save()
+            csv.close()
+
+            conf.save()
+    
+    # call load data
+    LoadData()
