a271cf369dc84a8a0c72ef0ebafa491e716ac96d
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-05 18:17:58 +0100

    sc

--- con/models/country.py
+++ con/models/country.py
@@ -3,6 +3,7 @@ from base.models import ClassModel
 
 class Country(ClassModel):
     name = models.CharField(max_length=100, null=False, blank=False)
+    code = models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
         return self.name
